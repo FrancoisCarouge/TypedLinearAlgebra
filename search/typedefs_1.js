f6d7bde5ff8fe2f3afa5c1bb24b2fed9cf45323d
FrancoisCarouge <FrancoisCarouge@users.noreply.github.com>
2025-08-09 03:07:33 +0000

    deploy: 6ce2358134fb7f4bd79d8c9e18b214f543d0da92

--- search/typedefs_1.js
+++ search/typedefs_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['element_0',['element',['../classfcarouge_1_1typed__matrix.xhtml#af6be5fe000ae7ae363a9a01fec688d57',1,'fcarouge::typed_matrix::element'],['../namespacefcarouge_1_1typed__linear__algebra__internal.xhtml#ae127796f29a91fe39cd110accd6c82c5',1,'fcarouge::typed_linear_algebra_internal::element']]]
+  ['element_0',['element',['../classfcarouge_1_1typed__matrix.xhtml#af6be5fe000ae7ae363a9a01fec688d57',1,'fcarouge::typed_matrix::element'],['../namespacefcarouge_1_1typed__linear__algebra__internal.xhtml#ae0dc82c981c93af3822ced28330e2071',1,'fcarouge::typed_linear_algebra_internal::element']]]
 ];
--- utility_8hpp.js
+++ utility_8hpp.js
@@ -1,12 +1,12 @@
 var utility_8hpp =
 [
-    [ "element", "utility_8hpp.xhtml#ae127796f29a91fe39cd110accd6c82c5", null ],
+    [ "element", "utility_8hpp.xhtml#ae0dc82c981c93af3822ced28330e2071", null ],
     [ "identity_index", "utility_8hpp.xhtml#a2963e691119d8b2ac7bdeac82688b5b9", null ],
     [ "product", "utility_8hpp.xhtml#a174364178a20af0590ffe5c549e13e66", null ],
     [ "quotient", "utility_8hpp.xhtml#ad9aae459061728b7d7b2ae70196f2028", null ],
     [ "repack", "utility_8hpp.xhtml#a8beb8d3741b310de71851a081557455e", null ],
     [ "tuple_n_type", "utility_8hpp.xhtml#a4dd57b09634fe03c0fd59b866657ad86", null ],
-    [ "underlying_t", "utility_8hpp.xhtml#a1233ce8915a8ea54b53c7912a477afdc", null ],
+    [ "underlying_t", "utility_8hpp.xhtml#a850ffce4253219f2b82c441cc4de8efc", null ],
     [ "for_constexpr", "utility_8hpp.xhtml#a9a86c642ba2610ed4f92a3d84baf5b00", null ],
     [ "size", "utility_8hpp.xhtml#aef655fff0379b424e1493f0e13f75682", null ]
 ];
