11d969c5d69c958ca5d0509c21d09504cb16978d
FrancoisCarouge <FrancoisCarouge@users.noreply.github.com>
2025-10-29 04:13:28 +0000

    deploy: 48162c5cb94c7c5b5fe01575440b6c16b2085079

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['main_0',['main',['../main_8cpp.xhtml#a8216c1645620cdb2f629cde3ac02ffef',1,'main.cpp']]]
+  ['main_0',['main',['../main_8cpp.xhtml#a8216c1645620cdb2f629cde3ac02ffef',1,'main.cpp']]],
+  ['make_5ftyped_5fmatrix_1',['make_typed_matrix',['../namespacefcarouge.xhtml#a4073b31d56e98e110bf4a993ed190c80',1,'fcarouge']]]
 ];
--- typed__linear__algebra_8hpp.js
+++ typed__linear__algebra_8hpp.js
@@ -11,6 +11,7 @@ var typed__linear__algebra_8hpp =
     [ "fcarouge::same_shape", "conceptfcarouge_1_1same__shape.xhtml", null ],
     [ "typed_column_vector", "typed__linear__algebra_8hpp.xhtml#a96fea71ab24114a61950988fdac23f58", null ],
     [ "typed_row_vector", "typed__linear__algebra_8hpp.xhtml#a7558e7ccc03031fb7de678cf4977e1bf", null ],
+    [ "make_typed_matrix", "typed__linear__algebra_8hpp.xhtml#a4073b31d56e98e110bf4a993ed190c80", null ],
     [ "operator*", "typed__linear__algebra_8hpp.xhtml#a37ad0c6ac7df201ab1ffbf86e277506b", null ],
     [ "operator==", "typed__linear__algebra_8hpp.xhtml#ac201eb6dbabe90e8e35d2efa46b7ad1f", null ],
     [ "cast", "typed__linear__algebra_8hpp.xhtml#aa6f7f6a8d1b65520e70b3e1330e880d2", null ]
