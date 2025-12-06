5a6858deda08d14d713acbec8c3d5fbf0d80815b
FrancoisCarouge <FrancoisCarouge@users.noreply.github.com>
2025-12-06 03:50:34 +0000

    deploy: 77724466567726a59d2d5597c266e9d209ae75bd

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['explicit_0',['explicit',['../classfcarouge_1_1typed__matrix.xhtml#a16857257759870c394e7038923b581e3',1,'fcarouge::typed_matrix::explicit(false) typed_matrix(const same_as_typed_matrix auto &amp;other)'],['../classfcarouge_1_1typed__matrix.xhtml#a0b1fe6127e30029715ed522aa6959c7b',1,'fcarouge::typed_matrix::explicit(false) typed_matrix(const same_as_typed_matrix auto &amp;&amp;other)'],['../classfcarouge_1_1typed__matrix.xhtml#ae8ec9e22e3a2c343032a1b861ada464d',1,'fcarouge::typed_matrix::explicit(false) typed_matrix(const element&lt; 0']]]
+  ['explicit_0',['explicit',['../classfcarouge_1_1typed__matrix.xhtml#a16857257759870c394e7038923b581e3',1,'fcarouge::typed_matrix::explicit(false) typed_matrix(const same_as_typed_matrix auto &amp;other)'],['../classfcarouge_1_1typed__matrix.xhtml#ab640b378259edec3a845ea08c11030e2',1,'fcarouge::typed_matrix::explicit(false) typed_matrix(same_as_typed_matrix auto &amp;&amp;other)'],['../classfcarouge_1_1typed__matrix.xhtml#ae8ec9e22e3a2c343032a1b861ada464d',1,'fcarouge::typed_matrix::explicit(false) typed_matrix(const element&lt; 0']]]
 ];
