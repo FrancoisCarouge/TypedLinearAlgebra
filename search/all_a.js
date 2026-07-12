21ab603dc606a0257303158f3cf602496181e6b1
FrancoisCarouge <FrancoisCarouge@users.noreply.github.com>
2026-07-12 02:56:06 +0000

    deploy: 54634242b1b4b35569d86df56c8f9dc7b05e10de

--- search/all_a.js
+++ search/all_a.js
@@ -3,7 +3,7 @@ var searchData=
   ['data_0',['data',['../classfcarouge_1_1typed__matrix.xhtml#a032880fe707e41d14c7a9dfa3f321fee',1,'fcarouge::typed_matrix']]],
   ['data_5fhandle_5ftype_1',['data_handle_type',['../structfcarouge_1_1benchmark_1_1anonymous__namespace_02mdspan__tuple__product_8cpp_03_1_1accessor.xhtml#afb84213644ee63d4e6c4aa364eeec20f',1,'fcarouge::benchmark::anonymous_namespace{mdspan_tuple_product.cpp}::accessor::data_handle_type'],['../structfcarouge_1_1benchmark_1_1anonymous__namespace_02typed__mdspan__tupled__product_8cpp_03_1_1accessor.xhtml#a1b01c0abdc27a6e0f98aa2898b120ef5',1,'fcarouge::benchmark::anonymous_namespace{typed_mdspan_tupled_product.cpp}::accessor::data_handle_type']]],
   ['deck_2',['Slide Deck',['../md_html_2typed__linear__algebra_2_r_e_a_d_m_e.xhtml',1,'']]],
-  ['declaration_3',['Declaration',['../md_html_2paper_2mp__units__eigen.xhtml#declaration',1,'Declaration'],['../index.xhtml#declaration-1',1,'Declaration']]],
+  ['declaration_3',['Declaration',['../index.xhtml#declaration',1,'Declaration'],['../md_html_2paper_2mp__units__eigen.xhtml#declaration-1',1,'Declaration']]],
   ['deg_5fc2_4',['deg_C2',['../namespacefcarouge.xhtml#a5a8a206bd21b1225ecfea4798c26d775',1,'fcarouge']]],
   ['deployment_20actions_5',['Continuous Integration &amp; Deployment Actions',['../index.xhtml#continuous-integration--deployment-actions',1,'']]],
   ['development_20build_20run_6',['Development Build &amp; Run',['../md__2github_2workspace_2_i_n_s_t_a_l_l.xhtml#development-build--run',1,'']]],
