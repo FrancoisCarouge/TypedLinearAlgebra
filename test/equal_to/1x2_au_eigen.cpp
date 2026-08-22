6b090ddf0391b70fd06700f50319ee90e7ebb6a5
FrancoisCarouge <francois.carouge@gmail.com>
2026-08-22 14:00:25 -0700

    [support] Aurora Au backend plug-in support

---
+++ test/equal_to/1x2_au_eigen.cpp
@@ -0,0 +1,65 @@
+/* Typed Linear Algebra
+Version 0.3.0
+https://github.com/FrancoisCarouge/TypedLinearAlgebra
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#include "fcarouge/linalg.hpp"
+
+#include <au/units/meters.hh>
+#include <au/units/seconds.hh>
+
+#include <cassert>
+
+namespace fcarouge::test {
+using representation = double;
+
+namespace {
+//! @test Verifies the equal to algorithm for a row vector of heterogeneous,
+//! non-trivial types.
+[[maybe_unused]] const auto test{[] -> int {
+  using position = au::QuantityD<au::Meters>;
+  using velocity = au::QuantityD<au::UnitQuotientT<au::Meters, au::Seconds>>;
+
+  const row_vector<representation, position, velocity> a{
+      au::meters(1.), au::meters(2.) / au::seconds(1.)};
+  const row_vector<representation, position, velocity> b{
+      au::meters(1.), au::meters(2.) / au::seconds(1.)};
+  const row_vector<representation, position, velocity> c{
+      au::meters(1.), au::meters(3.) / au::seconds(1.)};
+  const row_vector<representation, position, velocity> d{
+      au::meters(9.), au::meters(2.) / au::seconds(1.)};
+
+  assert(a == b);
+  assert(a != c);
+  assert(a != d);
+
+  return 0;
+}()};
+} // namespace
+} // namespace fcarouge::test
---
+++ test/multiplication/1x1_au_std.cpp
@@ -0,0 +1,73 @@
+/* Typed Linear Algebra
+Version 0.3.0
+https://github.com/FrancoisCarouge/TypedLinearAlgebra
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#include "fcarouge/linalg.hpp"
+
+#include <au/units/meters.hh>
+
+#include <cassert>
+#include <cstddef>
+#include <mdspan>
+
+namespace fcarouge::test {
+using representation = double;
+
+namespace {
+//! @test Verifies the singleton by singleton matrix multiplication operator.
+[[maybe_unused]] const auto test{[] -> int {
+  using length = au::QuantityD<au::Meters>;
+  using area = au::QuantityD<au::UnitPowerT<au::Meters, 2>>;
+
+  double storage_a{0.};
+  double storage_b{0.};
+  double storage_r{0.};
+
+  std::mdspan span_a{&storage_a, std::extents<std::size_t, 1, 1>{}};
+  std::mdspan span_b{&storage_b, std::extents<std::size_t, 1, 1>{}};
+  std::mdspan span_r{&storage_r, std::extents<std::size_t, 1, 1>{}};
+
+  row_vector<representation, length> a{span_a};
+  row_vector<representation, length> b{span_b};
+  row_vector<representation, area> r{span_r};
+
+  a = au::meters(2.);
+  b = au::meters(3.);
+  r = a * b;
+
+  assert(au::squared(au::meters)(6.) == r.at());
+  assert(au::squared(au::meters)(6.) == r[]);
+  assert(au::squared(au::meters)(6.) == r());
+  assert(au::squared(au::meters)(6.) == r);
+
+  return 0;
+}()};
+} // namespace
+} // namespace fcarouge::test
