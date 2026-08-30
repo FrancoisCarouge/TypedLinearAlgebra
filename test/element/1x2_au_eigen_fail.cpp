6c3399666266e2872f874fc83d14da73da89d1e3
FrancoisCarouge <francois.carouge@gmail.com>
2026-08-30 13:20:36 -0700

    [test] add a test matrix to help identify gaps

---
+++ test/element/1x2_au_eigen_fail.cpp
@@ -0,0 +1,60 @@
+/* Typed Linear Algebra
+Version 0.3.0
+https://github.com/FrancoisCarouge/TypedLinearAlgebra
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#include "fcarouge/linalg.hpp"
+
+#include <au/units/amperes.hh>
+#include <au/units/meters.hh>
+#include <au/units/newtons.hh>
+#include <au/units/seconds.hh>
+
+namespace fcarouge::test {
+namespace {
+//! @test Verifies a row vector, rank one, matrix element cannot be accessed
+//! with two indexes, as if it were a rank two matrix.
+[[maybe_unused]] const auto test{[] {
+  using au::symbols::m;
+  using au::symbols::N;
+  using au::symbols::A;
+
+  using matrix = matrix<double, std::tuple<decltype(1. * m)>,
+                        std::tuple<decltype(1. * N), decltype(1. / A)>>;
+
+  // Intended:
+  // using quantity = matrix::element<0>;
+
+  using quantity = matrix::element<0, 0>;
+  [[maybe_unused]] quantity value;
+
+  return 0;
+}()};
+} // namespace
+} // namespace fcarouge::test
---
+++ test/operator/subscript_3x3.cpp
@@ -0,0 +1,66 @@
+/* Typed Linear Algebra
+Version 0.3.0
+https://github.com/FrancoisCarouge/TypedLinearAlgebra
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#include "fcarouge/linalg.hpp"
+
+#include <cassert>
+
+namespace fcarouge::test {
+namespace {
+//! @test Verifies the operator subscript accessor.
+[[maybe_unused]] const auto test{[] -> int {
+  matrix<double, 3, 3> m{{1., 2., 3.}, {4., 5., 6.}};
+
+  assert((m[0, 0] == 1.));
+  assert((m[0, 1] == 2.));
+  assert((m[0, 2] == 3.));
+  assert((m[1, 0] == 4.));
+  assert((m[1, 1] == 5.));
+  assert((m[1, 2] == 6.));
+
+  m.at<0, 0>(11.);
+  m.at<0, 1>(12.);
+  m.at<0, 2>(13.);
+  m.at<1, 0>(14.);
+  m.at<1, 1>(15.);
+  m.at<1, 2>(16.);
+
+  assert((m[0, 0] == 11.));
+  assert((m[0, 1] == 12.));
+  assert((m[0, 2] == 13.));
+  assert((m[1, 0] == 14.));
+  assert((m[1, 1] == 15.));
+  assert((m[1, 2] == 16.));
+
+  return 0;
+}()};
+} // namespace
+} // namespace fcarouge::test
