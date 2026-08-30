3fc0e359a167f73c30528e254f8f3adcfe3d0740
FrancoisCarouge <francois.carouge@gmail.com>
2026-08-30 13:20:36 -0700

    [test] add a test matrix to help identify gaps

---
+++ test/element/2x1_mp_units_eigen_fail.cpp
@@ -0,0 +1,56 @@
+/* Typed Linear Algebra
+Version 0.3.0
+https://github.com/FrancoisCarouge/TypedLinearAlgebra
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#include "fcarouge/linalg.hpp"
+
+namespace fcarouge::test {
+namespace {
+using mp_units::si::unit_symbols::m;
+using mp_units::si::unit_symbols::N;
+using mp_units::si::unit_symbols::s;
+
+//! @test Verifies a column vector, rank one, matrix element cannot be
+//! accessed with no index, as if it were a rank zero matrix.
+[[maybe_unused]] const auto test{[] {
+  using matrix =
+      matrix<double, std::tuple<decltype(1. * m), decltype(1. * m / s)>,
+             std::tuple<decltype(1. * N)>>;
+
+  // Intended:
+  // using quantity = matrix::element<0>;
+
+  using quantity = matrix::element<>;
+  [[maybe_unused]] quantity value;
+
+  return 0;
+}()};
+} // namespace
+} // namespace fcarouge::test
