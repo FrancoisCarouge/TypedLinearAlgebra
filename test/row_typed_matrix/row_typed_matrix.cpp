5618676cbcb358296103083d8a01920f974676a9
FrancoisCarouge <francois.carouge@gmail.com>
2026-09-05 11:49:00 -0700

    [test] add row concept tests

---
+++ test/row_typed_matrix/row_typed_matrix.cpp
@@ -0,0 +1,67 @@
+/* Typed Linear Algebra
+Version 0.3.0
+https://github.com/FrancoisCarouge/TypedLinearAlgebra
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#include "fcarouge/eigen.hpp"
+#include "fcarouge/linalg.hpp"
+
+#include <tuple>
+
+namespace fcarouge::test {
+namespace {
+//! @test Verifies the row_typed_matrix concept over a built-in element type,
+//! Eigen backend.
+
+static_assert(row_typed_matrix<matrix<double, 1, 1>>);
+static_assert(row_typed_matrix<matrix<double, 1, 3>>);
+static_assert(row_typed_matrix<matrix<double, 1, 7>>);
+static_assert(row_typed_matrix<row_vector<double, 5>>);
+
+static_assert(row_typed_matrix<const matrix<double, 1, 4>>);
+static_assert(row_typed_matrix<matrix<double, 1, 4> &>);
+static_assert(row_typed_matrix<const matrix<double, 1, 4> &&>);
+
+static_assert(not row_typed_matrix<matrix<double, 3, 1>>);
+static_assert(not row_typed_matrix<matrix<double, 2, 2>>);
+static_assert(not row_typed_matrix<matrix<double, 3, 2>>);
+static_assert(not row_typed_matrix<column_vector<double, 3>>);
+
+static_assert(not row_typed_matrix<double>);
+static_assert(not row_typed_matrix<eigen::matrix<double, 1, 3>>);
+static_assert(not row_typed_matrix<std::tuple<double>>);
+
+static_assert(row_typed_matrix<matrix<double, 1, 1>> and
+              column_typed_matrix<matrix<double, 1, 1>>);
+static_assert(row_typed_matrix<matrix<double, 1, 6>> and
+              not column_typed_matrix<matrix<double, 1, 6>>);
+static_assert(column_typed_matrix<matrix<double, 6, 1>> and
+              not row_typed_matrix<matrix<double, 6, 1>>);
+} // namespace
+} // namespace fcarouge::test
