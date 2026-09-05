3ba2ef54a1f41144816242f72f8dbb102dfabbec
FrancoisCarouge <francois.carouge@gmail.com>
2026-09-05 14:08:43 -0700

    [tests] add rank concept tests

---
+++ test/rank_typed_matrix/mp_units_eigen.cpp
@@ -0,0 +1,73 @@
+/* Typed Linear Algebra
+Version 0.3.0
+https://github.com/FrancoisCarouge/TypedLinearAlgebra
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#include "fcarouge/linalg.hpp"
+
+#include <mp-units/systems/isq.h>
+#include <mp-units/systems/si.h>
+
+#include <functional>
+#include <tuple>
+
+namespace fcarouge::test {
+namespace {
+using mp_units::si::unit_symbols::m;
+using mp_units::si::unit_symbols::s;
+
+template <auto Reference>
+using quantity = mp_units::quantity<Reference, double>;
+using position = quantity<mp_units::isq::length[m]>;
+using velocity = quantity<mp_units::isq::velocity[m / s]>;
+
+//! @test Verifies the rank_typed_matrix concept over mp-units quantity indexes,
+//! Eigen backend.
+
+using singleton =
+    matrix<double, std::tuple<position>, std::tuple<std::identity>>;
+using row =
+    matrix<double, std::tuple<std::identity>, std::tuple<position, velocity>>;
+using column =
+    matrix<double, std::tuple<position, velocity>, std::tuple<std::identity>>;
+using plane = matrix<double, std::tuple<position, velocity>,
+                     std::tuple<position, velocity>>;
+
+static_assert(rank_typed_matrix<singleton, 0>);
+static_assert(rank_typed_matrix<row, 1>);
+static_assert(rank_typed_matrix<column, 1>);
+static_assert(rank_typed_matrix<plane, 2>);
+static_assert(rank_typed_matrix<const row &, 1>);
+
+static_assert(not rank_typed_matrix<singleton, 1>);
+static_assert(not rank_typed_matrix<row, 0>);
+static_assert(not rank_typed_matrix<row, 2>);
+static_assert(not rank_typed_matrix<plane, 1>);
+} // namespace
+} // namespace fcarouge::test
