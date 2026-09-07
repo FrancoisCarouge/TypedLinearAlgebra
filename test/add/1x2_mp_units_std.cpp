3a6645995d51d2e2379ae69c3e79751ebd613fc6
FrancoisCarouge <francois.carouge@gmail.com>
2026-09-07 13:31:25 -0700

    [test] split addition and add tests

--- test/add/1x2_mp_units_std.cpp
+++ test/add/1x2_mp_units_std.cpp
@@ -34,47 +34,51 @@ For more information, please refer to <https://unlicense.org> */
 #include <cassert>
 #include <cstddef>
 #include <mdspan>
-#include <type_traits>
 
 namespace fcarouge::test {
+using literals::operator""_i;
 using representation = double;
 
 template <auto QuantityReference>
 using quantity = mp_units::quantity<QuantityReference, representation>;
 
 using mp_units::si::unit_symbols::m;
+using mp_units::si::unit_symbols::s;
 
 namespace {
-//! @test Verifies the singleton by singleton matrix addition operator.
+//! @test Verifies the `add` function with non-trivial types.
 [[maybe_unused]] const auto test{[] -> int {
-  using length = quantity<mp_units::isq::length[m]>;
+  using position = quantity<mp_units::isq::length[m]>;
+  using velocity = quantity<mp_units::isq::velocity[m / s]>;
 
-  double storage_a{0.};
-  double storage_b{0.};
-  double storage_r{0.};
+  double storage_a[]{0., 0.};
+  double storage_b[]{0., 0.};
+  double storage_r[]{0., 0.};
 
-  std::mdspan span_a{&storage_a, std::extents<std::size_t, 1, 1>{}};
-  std::mdspan span_b{&storage_b, std::extents<std::size_t, 1, 1>{}};
-  std::mdspan span_r{&storage_r, std::extents<std::size_t, 1, 1>{}};
+  std::mdspan span_a{&storage_a[0], std::extents<std::size_t, 1, 2>{}};
+  std::mdspan span_b{&storage_b[0], std::extents<std::size_t, 1, 2>{}};
+  std::mdspan span_r{&storage_r[0], std::extents<std::size_t, 1, 2>{}};
 
-  row_vector<representation, length> a{span_a};
-  row_vector<representation, length> b{span_b};
-  row_vector<representation, length> r{span_r};
+  row_vector<representation, position, velocity> a{span_a};
+  row_vector<representation, position, velocity> b{span_b};
+  row_vector<representation, position, velocity> r{span_r};
 
-  a = 2. * m;
-  b = 3. * m;
+  a.at<0_i>(1. * m);
+  a.at<1_i>(2. * m / s);
+  b.at<0_i>(3. * m);
+  b.at<1_i>(4. * m / s);
+
   add(a, b, r);
 
-  assert(5. * m == r);
-  assert(5. * m == r());
-  assert(5. * m == r[]);
-  assert(5. * m == r.at());
-  assert(5. * m == r.at<>());
+  assert(4. * m == r.at<0>());
+  assert(4. * m == r.at<0_i>());
+  assert(4. * m == r[0_i]);
+  assert(4. * m == r(0_i));
 
-  static_assert(not std::is_reference_v<decltype(r())>);
-  static_assert(not std::is_reference_v<decltype(r[])>);
-  static_assert(not std::is_reference_v<decltype(r.at())>);
-  static_assert(not std::is_reference_v<decltype(r.at<>())>);
+  assert(6. * m / s == r.at<1>());
+  assert(6. * m / s == r.at<1_i>());
+  assert(6. * m / s == r[1_i]);
+  assert(6. * m / s == r(1_i));
 
   return 0;
 }()};
