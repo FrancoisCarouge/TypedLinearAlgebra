d99fb7bf523e1bebbbd45a80c1b9b5d03764dbd5
FrancoisCarouge <francois.carouge@gmail.com>
2026-09-19 23:15:39 -0700

    [test] more substraction tests

---
+++ test/substraction/1x1_chrono_std_fail.cpp
@@ -0,0 +1,75 @@
+/* Typed Linear Algebra
+Version 0.4.0
+https://github.com/FrancoisCarouge/TypedLinearAlgebra
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#include "fcarouge/linalg.hpp"
+
+#include <cassert>
+#include <chrono>
+#include <cstddef>
+#include <mdspan>
+
+namespace fcarouge::test {
+using representation = double;
+
+namespace {
+//! @test Verifies subtracting two time points yields a duration, not another
+//! time point.
+[[maybe_unused]] const auto test{[] {
+  using instant =
+      std::chrono::time_point<std::chrono::system_clock,
+                              std::chrono::duration<representation>>;
+
+  // Intended: a duration singleton.
+  // row_vector<representation, std::chrono::duration<representation>>
+  // a{span_a};
+
+  representation storage_a{0.};
+  representation storage_b{0.};
+  representation storage_r{0.};
+
+  std::mdspan span_a{&storage_a, std::extents<std::size_t, 1, 1>{}};
+  std::mdspan span_b{&storage_b, std::extents<std::size_t, 1, 1>{}};
+  std::mdspan span_r{&storage_r, std::extents<std::size_t, 1, 1>{}};
+
+  row_vector<representation, instant> a{span_a};
+  row_vector<representation, instant> b{span_b};
+  row_vector<representation, instant> r{span_r};
+
+  a = instant{};
+  b = instant{};
+  r = a - b;
+
+  assert(instant{} == r.at());
+
+  return 0;
+}()};
+} // namespace
+} // namespace fcarouge::test
